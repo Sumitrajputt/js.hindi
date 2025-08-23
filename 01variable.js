392dfc326d310fc56a2a5d6786ba8d6fa2b51ac5
Sumit kumar singh <sumitkumarsingh8677@gmail.com>
2025-08-23 11:42:40 +0530

    Create 01variable.js
    23/08/2025 11:42am

---
+++ 01variable.js
@@ -0,0 +1,12 @@
+const accountID = 84566
+let accountEmail="sumitkumarsingh8677@gmail.com"
+var accountpassword = "54343"
+acccountcity ="siwan"
+//account 2//
+accountEmail= "sumit@gmail.com"
+accountpassword ="4975739"  
+acccountcity ="bihar"
+
+console.log(accountID);
+console.table([accountEmail,accountEmail,accountpassword,acccountcity])
+
